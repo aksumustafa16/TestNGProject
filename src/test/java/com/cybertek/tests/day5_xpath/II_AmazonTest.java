260b836597e4d43f68413218fceac8422037dd31
musaksu <musaksu@gmail.com>
2023-01-02 22:01:15 +0100

    CSS Selector

--- src/test/java/com/cybertek/tests/day5_xpath/II_AmazonTest.java
+++ src/test/java/com/cybertek/tests/day5_xpath/II_AmazonTest.java
@@ -1,11 +1,11 @@
-package com.cybertek.tests.day_5_xpath;
+package com.cybertek.tests.day5_xpath;
 
 import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class AmazonTest {
+public class II_AmazonTest {
     public static void main(String[] args) {
         WebDriver driver = WebDriverFactory.getDriver("chrome");
         driver.get("https://www.amazon.com");
