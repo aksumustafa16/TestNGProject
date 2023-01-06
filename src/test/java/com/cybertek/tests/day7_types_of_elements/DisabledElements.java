731591dccbe93b2aa48b276ec5e333e6a4c5127e
musaksu <musaksu@gmail.com>
2023-01-06 22:32:34 +0100

    Disabled Elements

--- src/test/java/com/cybertek/tests/day7_types_of_elements/DisabledElements.java
+++ src/test/java/com/cybertek/tests/day7_types_of_elements/DisabledElements.java
@@ -1,4 +1,20 @@
 package com.cybertek.tests.day7_types_of_elements;
 
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class DisabledElements {
+
+    @Test
+    public void test1(){
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.get("http://practice.cybertekschool.com/radio_buttons");
+        WebElement greenRadioButton = driver.findElement(By.id("green"));
+        Assert.assertFalse(greenRadioButton.isEnabled(),"Verify green button is NOT enabled");
+    }
 }
