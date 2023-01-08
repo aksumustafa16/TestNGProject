344602863c8711bf3ab091b815da7415c110efa7
musaksu <musaksu@gmail.com>
2023-01-08 22:56:55 +0100

    List Of Elements

--- src/test/java/com/cybertek/tests/day7_types_of_elements/ListOfElements.java
+++ src/test/java/com/cybertek/tests/day7_types_of_elements/ListOfElements.java
@@ -11,16 +11,16 @@ import java.util.List;
 
 public class ListOfElements {
     @Test
-    public void test1(){
+    public void test1() {
         WebDriver driver = WebDriverFactory.getDriver("chrome");
         driver.get("http://practice.cybertekschool.com/multiple_buttons");
 
         //save our webelements inside the list
-        List<WebElement> buttons =  driver.findElements(By.tagName("button"));
+        List<WebElement> buttons = driver.findElements(By.tagName("button"));
         System.out.println("buttons.size() = " + buttons.size());
 
         //verify we have 6 buttons
-        Assert.assertEquals(buttons.size(),6);
+        Assert.assertEquals(buttons.size(), 6);
 
         //loop for each button, we can implement
         for (WebElement button : buttons) {
@@ -29,8 +29,19 @@ public class ListOfElements {
 
         //click on the second button from the list
         buttons.get(1).click();
-
-
-
     }
+        @Test
+        public void test2(){
+            WebDriver driver = WebDriverFactory.getDriver("chrome");
+            driver.get("http://practice.cybertekschool.com/multiple_buttons");
+
+            //It is not giving error for WebElements for list just giving empty list
+            List<WebElement> buttons = driver.findElements(By.tagName("buttonkjdhfkldjfkdj"));
+            System.out.println("buttons.size() = " + buttons.size());
+
+            //it is giving error for Webelement
+            //WebElement button = driver.findElement(By.tagName("buttonkjdhfkldjfkdj"));
+
+
+        }
 }
