d3e923af02ad42f9d0dacebfe30db8c78ee8b3a6
musaksu <musaksu@gmail.com>
2023-01-08 22:31:45 +0100

    Display Demo Test

---
+++ src/test/java/com/cybertek/tests/day7_types_of_elements/DisplayDemoTest.java
@@ -0,0 +1,31 @@
+package com.cybertek.tests.day7_types_of_elements;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DisplayDemoTest {
+
+    @Test
+    public void test1() throws InterruptedException {
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.get("http://practice.cybertekschool.com/dynamic_loading/1");
+
+        WebElement usernameInput = driver.findElement(By.id("username"));
+
+        System.out.println("usernameInput.isDisplayed() = " + usernameInput.isDisplayed());
+
+        //verify that username not displayed in the screen,
+        Assert.assertFalse(usernameInput.isDisplayed());
+        //click start button
+        driver.findElement(By.xpath("//button")).click();
+        Thread.sleep(5000);
+        //verify username displayed on the screen
+        Assert.assertTrue(usernameInput.isDisplayed());
+
+    }
+}
