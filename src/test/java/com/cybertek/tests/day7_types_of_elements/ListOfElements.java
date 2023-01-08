5a183dc493501b3f8483bc06f614d39663b6c927
musaksu <musaksu@gmail.com>
2023-01-08 22:49:35 +0100

    List Of Elements

--- src/test/java/com/cybertek/tests/day7_types_of_elements/ListOfElements.java
+++ src/test/java/com/cybertek/tests/day7_types_of_elements/ListOfElements.java
@@ -4,6 +4,7 @@ import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -13,7 +14,21 @@ public class ListOfElements {
     public void test1(){
         WebDriver driver = WebDriverFactory.getDriver("chrome");
         driver.get("http://practice.cybertekschool.com/multiple_buttons");
+
+        //save our webelements inside the list
         List<WebElement> buttons =  driver.findElements(By.tagName("button"));
+        System.out.println("buttons.size() = " + buttons.size());
+
+        //verify we have 6 buttons
+        Assert.assertEquals(buttons.size(),6);
+
+        //loop for each button, we can implement
+        for (WebElement button : buttons) {
+            System.out.println(button.isDisplayed());
+        }
+
+        //click on the second button from the list
+        buttons.get(1).click();
 
 
 
