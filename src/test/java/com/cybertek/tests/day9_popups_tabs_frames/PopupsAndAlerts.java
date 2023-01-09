9a474bccab899fb64f86737662918f327edf2458
musaksu <musaksu@gmail.com>
2023-01-09 21:13:56 +0100

    Click Popups

--- src/test/java/com/cybertek/tests/day9_popups_tabs_frames/PopupsAndAlerts.java
+++ src/test/java/com/cybertek/tests/day9_popups_tabs_frames/PopupsAndAlerts.java
@@ -1,27 +1,34 @@
 package com.cybertek.tests.day9_popups_tabs_frames;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class PopupsAndAlerts {
 
+    WebDriver driver;
+
     @BeforeMethod
     public void setUpMethod(){
-        WebDriver driver = WebDriverFactory.getDriver("chrome");
+         driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
     }
 
     @AfterMethod
     public void afterMethod(){
-        driver.quit;
+       // driver.quit();
     }
 
     @Test
-    public void test1(){
-        WebDriver driver = WebDriverFactory.getDriver("chrome");
+    public void test1() throws InterruptedException {
         driver.get("https://www.primefaces.org/showcase/ui/overlay/confirmDialog.xhtml?jfwid=4c754");
-        driver.quit();
+
+        //locate and click Confirm button
+        driver.findElement(By.id("j_idt343:j_idt344")).click();
+        driver.findElement(By.xpath("//button[.='Yes']")).click();
     }
 }
