2a8c54d784efd592c5bf2c92e1c11e73c2f2518e
musaksu <musaksu@gmail.com>
2023-01-24 16:00:56 +0100

    Implicit Wait

---
+++ src/test/java/com/cybertek/tests/day_10_sync/ImplicitWaitExample.java
@@ -0,0 +1,38 @@
+package com.cybertek.tests.day_10_sync;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class ImplicitWaitExample {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        driver.quit();
+    }
+
+    @Test
+    public void test1() {
+
+        driver.get("http://practice.cybertekschool.com/dynamic_loading/4");
+
+        //wait up to 20 second
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+        String message = driver.findElement(By.cssSelector("#finish>h4")).getText();
+        System.out.println("message = " + message);
+    }
+
+}
