6940afa1cf177c10c397d63ad9eb1b38e9c3cc37
musaksu <musaksu@gmail.com>
2023-02-01 13:29:44 +0100

    singleton

--- src/test/java/com/cybertek/tests/day14_properties_driver_tests/PropertiesTest.java
+++ src/test/java/com/cybertek/tests/day14_properties_driver_tests/PropertiesTest.java
@@ -1,6 +1,8 @@
 package com.cybertek.tests.day14_properties_driver_tests;
 
 import com.cybertek.utilities.ConfigurationReader;
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 public class PropertiesTest {
 
@@ -8,5 +10,8 @@ public class PropertiesTest {
     public void test1(){
         String browser = ConfigurationReader.get("browser");
         System.out.println("browser = " + browser);
+        System.out.println(ConfigurationReader.get("driver_username"));
+        System.out.println(ConfigurationReader.get("storemanager_username"));
+
     }
 }
