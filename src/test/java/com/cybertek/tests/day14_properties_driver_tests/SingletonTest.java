38572685380ae70c8529fb8723ee27c0a498be1a
musaksu <musaksu@gmail.com>
2023-02-01 23:22:41 +0100

    singleton driver

--- src/test/java/com/cybertek/tests/day14_properties_driver_tests/SingletonTest.java
+++ src/test/java/com/cybertek/tests/day14_properties_driver_tests/SingletonTest.java
@@ -1,5 +1,8 @@
 package com.cybertek.tests.day14_properties_driver_tests;
 
+import com.cybertek.utilities.Driver;
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 public class SingletonTest {
@@ -11,5 +14,27 @@ public class SingletonTest {
 
         System.out.println("s1 = " + s1);
         System.out.println("s2 = " + s2);
+    }
+
+    @Test
+    public void test1(){
+
+        WebDriver driver = Driver.get();
+
+        driver.get("https://www.google.com");
+
+        Driver.closeDriver();
+
+    }
+
+    @Test
+    public void test2(){
+
+        WebDriver driver = Driver.get();
+
+        driver.get("https://www.amazon.com");
+
+        Driver.closeDriver();
+
     }
 }
