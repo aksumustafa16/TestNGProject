9a82b833f12e8dd77e52853a24f331a47639b45f
musaksu <musaksu@gmail.com>
2023-02-02 16:24:35 +0100

    created pom classes

--- src/test/java/com/cybertek/tests/day16_pom/NegativeLoginTest.java
+++ src/test/java/com/cybertek/tests/day16_pom/NegativeLoginTest.java
@@ -1,15 +1,28 @@
 package com.cybertek.tests.day16_pom;
 
+import com.cybertek.pages.LoginPage;
 import com.cybertek.tests.TestBase;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class NegativeLoginTest extends TestBase {
 
     @Test
     public void wrongPasswordTest() {
+        LoginPage loginPage = new LoginPage();
+        loginPage.usernameInput.sendKeys("User1");
+        loginPage.passwordInput.sendKeys("samepassword");
+        loginPage.loginBtn.click();
+        Assert.assertTrue(driver.getCurrentUrl().equals("https://qa3.vytrack.com/user/login"));
+    }
 
-
-
+    @Test
+    public void wrongUsernameTest(){
+        LoginPage loginPage = new LoginPage();
+        loginPage.usernameInput.sendKeys("someusername");
+        loginPage.passwordInput.sendKeys("UserUser123");
+        loginPage.loginBtn.click();
+        Assert.assertTrue(driver.getCurrentUrl().equals("https://qa3.vytrack.com/user/login"));
 
     }
 }
