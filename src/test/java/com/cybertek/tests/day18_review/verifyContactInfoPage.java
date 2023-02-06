38f31bf3800e7a3fdaa78e8b892f89d3620a57ce
musaksu <musaksu@gmail.com>
2023-02-06 22:05:22 +0100

    contact info page

--- src/test/java/com/cybertek/tests/day18_review/verifyContactInfoPage.java
+++ src/test/java/com/cybertek/tests/day18_review/verifyContactInfoPage.java
@@ -4,6 +4,7 @@ import com.cybertek.pages.*;
 import com.cybertek.tests.TestBase;
 import com.cybertek.utilities.ConfigurationReader;
 import org.checkerframework.checker.units.qual.C;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class verifyContactInfoPage extends TestBase {
@@ -19,9 +20,9 @@ public class verifyContactInfoPage extends TestBase {
         extentLogger.info("username" + username);
         extentLogger.info("password" + password);
         extentLogger.info("Login as a Sales Manager");
-        loginPage.login(username,password);
+        loginPage.login(username, password);
 
-        new DashboardPage().navigateToModule("Customers","Contacts");
+        new DashboardPage().navigateToModule("Customers", "Contacts");
         extentLogger.info("Navigate to Customers -- Contacts");
 
         ContactsPage contactsPage = new ContactsPage();
@@ -30,6 +31,22 @@ public class verifyContactInfoPage extends TestBase {
 
         extentLogger.info("Click on mbrackstone9@example.com email");
         contactsPage.getContactEmail("mbrackstone9@example.com").click();
+
+        ContactInfoPage contactInfoPage = new ContactInfoPage();
+
+        String expectedFullName = "Mariam Brackstone";
+        String actualFullName = contactInfoPage.fullName.getText();
+
+        extentLogger.info("Verify full name is " + expectedFullName);
+        Assert.assertEquals(actualFullName, expectedFullName, "Verify fullName");
+
+        extentLogger.info("Verify email is mbrackstone9@example.com ");
+        Assert.assertEquals(contactInfoPage.email.getText(), "mbrackstone9@example.com", "Verify email");
+
+        extentLogger.info("Verify email is +18982323434");
+        Assert.assertEquals(contactInfoPage.phone.getText(), "+18982323434", "Verify phone");
+
+        extentLogger.pass("PASS: Contact Info Test");
 
     }
 }
